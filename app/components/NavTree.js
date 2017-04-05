86820b4aafe7c3a68066009d3be8c2d1fdca1add
Marko Stijak <mstijak@gmail.com>
2017-04-05 13:53:59 +0200

    Upgrade packages. Expand all nav

--- app/components/NavTree.js
+++ app/components/NavTree.js
@@ -1,27 +1,27 @@
 export default [{
     topic: 'Dashboard',
-    expanded: false,
+    expanded: true,
     articles: [
         {title: 'Sales', url: '~/dashboards/sales'},
         {title: 'Web Analytics', url: '~/dashboards/web-analytics'},
     ]
 }, {
     topic: 'Admin',
-    expanded: false,
+    expanded: true,
     articles: [
         {title: 'Contacts', url: '~/admin/contacts'},
         {title: 'Orders', url: '~/admin/orders'},
     ]
 }, {
     topic: 'Demos',
-    expanded: false,
+    expanded: true,
     articles: [
         {title: 'Todo List', url: '~/demos/todo'},
         {title: 'GitHub Explorer', url: '~/demos/github'}
     ]
 }, {
     topic: 'Meta',
-    expanded: false,
+    expanded: true,
     articles: [
         {title: 'Open Source', url: '~/meta/open-source-software'},
     ]
